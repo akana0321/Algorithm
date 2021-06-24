9d3fdeb592fcb77667e888f65f9dcf6a7b6e9efe
akana0321 <akana0321@gmail.com>
2021-06-24 21:00:22 +0900

    20210622

--- 이것이 코딩테스트다 with 파이썬/Greedy/law_of_large_numbers.py
+++ 이것이 코딩테스트다 with 파이썬/Greedy/law_of_large_numbers.py
@@ -4,8 +4,8 @@ n, m, k = map(int, input().split())
 data = list(map(int, input().split()))
 
 data.sort() # 입력 받는 수 정렬
-first = data[n-1] # 가장 큰 수
-second = data[n-2] # 두 번째로 큰 수
+first = data[n - 1] # 가장 큰 수
+second = data[n - 2] # 두 번째로 큰 수
 
 result = 0
 
