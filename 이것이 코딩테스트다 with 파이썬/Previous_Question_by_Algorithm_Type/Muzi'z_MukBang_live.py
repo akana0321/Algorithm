170680527dab72db7f4f7019c5dfefec9218dff7
akana0321 <akana0321@gmail.com>
2021-07-19 10:09:10 +0900

    20210719 - hard....

--- 이것이 코딩테스트다 with 파이썬/Previous_Question_by_Algorithm_Type/Muzi'z_MukBang_live.py
+++ 이것이 코딩테스트다 with 파이썬/Previous_Question_by_Algorithm_Type/Muzi'z_MukBang_live.py
@@ -20,20 +20,32 @@
             k는 1 이상 2 x 10^13 이하의 자연수이다.
      - 입력 예시 : food_times = [3, 1, 2] / k = 5 -> result = 1
 """
+import heapq
 def solution(food_times, k):
-    orders = []
+    # 전체 음식을 먹는 시간보다 k가 크거나 같다면 -1
 
-    for i in range(0, sum(food_times)+1):
-        if i == k + 1:
-            break
+    if sum(food_times) <= k:
+        return -1
 
-        if food_times[i % len(food_times)] != 0:
-            food_times[i % len(food_times)] -= 1
-            orders.append(i % len(food_times))
+    # 시간이 작은 음식부터 빼야 하므로 우선순위 큐를 이용
+    q = []
+    for i in range(len(food_times)):
+        # (음식 시간, 음식 번호) 형태로 우선순위 큐에 삽입
+        heapq.heappush(q, (food_times[i], i + 1))
 
-    print(food_times)
-    print(orders)
-    answer = orders[-1]
-    return answer+1
+    sum_value = 0 # 먹기 위해 사용한 시간
+    previous = 0 # 직전에 다 먹은 음식 시간
+    length = len(food_times) # 남은 음식의 개수
+
+    # sum_value + (현재의 음식 시간 - 이전 음식 시간) * 현재 음식 개수와 k 비교
+    while sum_value + ((q[0][0] - previous) * length) <= k:
+        now = heapq.heappop(q)[0]
+        sum_value += (now - previous) * length
+        length -= 1 # 다 먹은 음식 제외
+        previous = now # 이전 음식 시간 재설정
+
+    # 남은 음식 중에서 몇 번째 음식인지 확인하여 출력
+    result = sorted(q, key=lambda x: x[1])
+    return result[(k - sum_value) % length][1]
 
 print(solution([3, 1, 2], 5))
