ac81cba96876d26b0a5011c1460134e19ebeebf3
Yunse Oh <akana0321@gmail.com>
2025-05-03 22:31:34 +0900

    [leetcode_easy] Valid Parentheses - Deque 사용

--- java/leetcode/src/main/java/easy/validparenthese/Solution.java
+++ java/leetcode/src/main/java/easy/validparenthese/Solution.java
@@ -13,41 +13,30 @@ public class Solution {
 			return false;
 		}
 
-		List<Character> brackets = new ArrayList<>();
+		// 괄호 Deque
+		Deque<Character> bracketsDeque = new ArrayDeque<>();
+		// 괄호 쌍
+		Map<Character, Character> bracketPairs = Map.of(
+						'(', ')',
+						'{', '}',
+						'[', ']'
+		);
 
-		for (int i = 0; i < s.length(); i++) {
-			char current = s.charAt(i);
-
-			// 여는 괄호일 경우 닫는 괄호로 목록에 추가한다.
-			if (current == '(') {
-				brackets.add(')');
-			} else if (current == '[') {
-				brackets.add(']');
-			} else if (current == '{') {
-				brackets.add('}');
+		for (char current : s.toCharArray()) {
+			if (bracketPairs.containsKey(current)) {
+				// 여는 괄호일 경우 Deque에 담는다.
+				bracketsDeque.push(bracketPairs.get(current));
+			} else if (bracketPairs.containsValue(current)) {
+				// 닫는 괄호일 경우 Deque.pop() 값과 일치하지 않을 경우 false를 반환한다.
+				if (bracketsDeque.isEmpty() || bracketsDeque.pop() != current) {
+					return false;
+				}
 			} else {
-				// 목록의 크기가 0일 경우 false를 반환한다.
-				if (brackets.size() == 0) {
-					return false;
-				}
-
-				// 목록의 마지막 괄호를 가져온다.
-				char lastBracket = brackets.get(brackets.size() - 1);
-				// 현재 괄호와 목록의 마지막 괄호가 다를 경우 false를 반환한다.
-				if (current != lastBracket) {
-					return false;
-				}
-
-				// 마지막 괄호를 제거한다.
-				brackets.remove(brackets.size() - 1);
+				// 이외의 값이 들어올 결우 false를 반환한다.
+				return false;
 			}
 		}
 
-		// 문자열을 순회했는데 목록이 남았다면 false를 반환한다.
-		if (brackets.size() != 0) {
-			return false;
-		}
-
-		return true;
+		return bracketsDeque.isEmpty();
 	}
 }
