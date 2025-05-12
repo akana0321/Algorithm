3c6c2c22cbbc47c0205a0ab54d685c9d3acf320d
Yunse Oh <akana0321@gmail.com>
2025-05-12 23:14:47 +0900

    [leetcode_easy] Remove Duplicates from Sorted Array 개선

--- java/leetcode/src/main/java/easy/removeduplicatesfromsortedarray/Solution.java
+++ java/leetcode/src/main/java/easy/removeduplicatesfromsortedarray/Solution.java
@@ -5,28 +5,20 @@ package easy.removeduplicatesfromsortedarray;
  *  https://leetcode.com/problems/remove-duplicates-from-sorted-array
  */
 class Solution {
-    public int removeDuplicates(int[] nums) {
-        // 중복 갯수
-        int duplicatedCount = 0;
+	public int removeDuplicates(int[] nums) {
+		// 현재 유효한 index
+		int validIndex = 0;
+		for (int i = 0; i < nums.length; i++) {
+			// 현재 유효한 index의 있는 값과 i번째에 있는 값이 다를 경우
+			if (nums[i] != nums[validIndex]) {
+				// 유효한 index를 하나 늘린다.
+				// validIndex++;
+				// 해당 값을 증가한 유효한 index 위치에 담는다.
+				nums[++validIndex] = nums[i];
+			}
+		}
 
-        // bubble sorting을 진행하는데 중복값의 경우는 최대값으로 설정한다.
-        for (int i = 0; i < nums.length; i++) {
-            for (int j = i + 1; j < nums.length; j++) {
-                // 중복값일 경우 최대값을 주어 맨 뒤로 보낸다.
-                if (nums[i] != Integer.MAX_VALUE && nums[i] == nums[j]) {
-                    nums[j] = Integer.MAX_VALUE;
-                    duplicatedCount += 1;
-                }
-
-                if (nums[i] > nums[j]) {
-                    int temp = nums[i];
-                    nums[i] = nums[j];
-                    nums[j] = temp;
-                }
-            }
-        }
-
-        // 유효한 범위의 index를 반환한다.
-        return nums.length - duplicatedCount;
-    }
+		// 길이를 반환해야 하므로 1을 더한다.
+		return validIndex + 1;
+	}
 }
