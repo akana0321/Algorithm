a0b812cbff6cea500860c805ba5309d0589e9a5d
Yunse Oh <akana0321@gmail.com>
2025-05-31 17:41:01 +0900

    [leetcode_easy] Minimum Depth of Binary Tree 리팩토링

--- java/leetcode/src/main/java/easy/minimumdepthofbinarytree/Solution.java
+++ java/leetcode/src/main/java/easy/minimumdepthofbinarytree/Solution.java
@@ -8,31 +8,19 @@ import easy.utill.TreeNode;
  */
 class Solution {
 	public int minDepth(TreeNode root) {
-		return this.getHeight(root);
-	}
-
-	public int getHeight(TreeNode node) {
-		int height = 0;
-
-		// 전달 받은 node가 null일 경우 높이는 0
-		if (node == null) {
-			return height;
+		if (root == null) {
+			return 0;
 		}
 
-		// 좌, 우 높이를 구한다.
-		int left = this.getHeight(node.left);
-		int right = this.getHeight(node.right);
+		int left = minDepth(root.left);
+		int right = minDepth(root.right);
 
-		// 한 쪽의 높이만 0일 경우는 아닌 쪽을, 둘 다 높이가 0이 아닐 경우는 작은 값을 준다.
-		if (left == 0 && right != 0) {
-			height = right;
-		} else if (left != 0 && right == 0) {
-			height = left;
-		} else {
-			height = Math.min(left, right);
+		// 한쪽 자식만 존재하는 경우 → 존재하는 쪽으로 깊이 계산
+		if (left == 0 || right == 0) {
+			return left + right + 1;
 		}
 
-		// 1(현재 노드) + height를 반환한다.
-		return 1 + height;
+		// 양쪽 자식 모두 존재하는 경우 → 더 얕은 쪽 선택
+		return Math.min(left, right) + 1;
 	}
 }
