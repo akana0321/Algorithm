da10ff7f61ae8a527ff4a5eb8ec259f272044e75
Yunse Oh <akana0321@gmail.com>
2026-04-03 17:51:43 +0900

    [boj] [silver] 1874 스택수열 하던 중

---
+++ java/baekjoon/src/main/java/silver/P1874.java
@@ -0,0 +1,22 @@
+package silver;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Stack;
+
+/**
+ * 1874 스택수열
+ * https://www.acmicpc.net/problem/1874
+ */
+public class P1874 {
+    public static void run() throws Exception {
+        StringBuilder result = new StringBuilder();
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+        Stack<Integer> stack = new Stack<>();
+
+        System.out.println(result);
+    }
+}
