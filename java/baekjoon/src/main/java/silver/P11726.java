228bd4775c4fb68a91effeb6c45940ac98b911cc
Yunse Oh <akana0321@gmail.com>
2026-04-09 20:30:05 +0900

    [boj] [silver] 11726 2xn 타일링 생각중

---
+++ java/baekjoon/src/main/java/silver/P11726.java
@@ -0,0 +1,16 @@
+package silver;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+/**
+ * 11726 2xn 타일링
+ * https://www.acmicpc.net/problem/11726
+ */
+public class P11726 {
+    public static void run() throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n =  Integer.parseInt(br.readLine());
+    }
+}
